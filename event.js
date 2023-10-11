6f85f5da8d9a6b142d96c1b08f999969af796b59
demo <ynarendra0617@gmail.com>
2023-10-11 10:06:44 +0530

    domevents

---
+++ event.js
@@ -0,0 +1,74 @@
+// let btn=document.getElementById("dd")
+// let list=document.getElementById("hidden-content")
+
+//& click,mouseover,mouseout,mousedown,mouseup
+
+// btn.addEventListener("click",()=>{
+//     if (list.style.display=="none") {
+//         list.style.display="block"
+//     }else{
+//         list.style.display="none"
+//     }
+// })
+
+// btn.addEventListener("mouseover",()=>{
+//     list.style.display="block"
+// })
+
+// btn.addEventListener("mouseout",()=>{
+//     list.style.display="none"
+// })
+
+// let h=document.getElementById("heart")
+// h.addEventListener("mousedown",()=>{
+//     h.innerHTML="❤️"
+// })
+//  h.addEventListener("mouseup",()=>{
+//     h.innerHTML="💕"
+//  })
+
+//! contextmenu----- rightclick
+
+// let q=document.getElementById("q");
+// q.addEventListener("contextmenu",()=>{
+//     q.innerHTML="The best traning platform"
+// })
+
+
+//! double click
+// let h1=document.querySelector("#db")
+// h1.addEventListener("dblclick",()=>{
+//     h1.innerHTML+="How r u all"
+//     document.body.style.color="red"
+// })
+
+//! submit
+// let form=document.getElementById("myform")
+// let btn=document.querySelector("button")
+
+// form.addEventListener("submit",()=>{
+//     document.body.style.background="green"
+//     alert("submitted")
+// })
+
+//! ket events
+
+// document.addEventListener("keydown",(e)=>{
+//     console.log("key down", e.key)
+// })
+
+// document.addEventListener("keyup",(e)=>{
+//     console.log("key up", e.key)
+// })
+
+// document.addEventListener("keypress",(e)=>{
+//     console.log("key pressed", e.key)
+// })
+
+
+// let form=document.getElementById("myform")
+// let btn=document.querySelector("button")
+
+// form.addEventListener("click",(e)=>{
+//     e.preventDefault()
+// })
